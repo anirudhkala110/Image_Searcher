245e000f028b4c894f74e943c343471fbdbeea3a
Anirudh Kala <122237860+anirudhkala110@users.noreply.github.com>
2023-10-25 02:53:20 +0530

    Update ImageSearch.jsx

--- src/Components/ImageSearch.jsx
+++ src/Components/ImageSearch.jsx
@@ -11,7 +11,7 @@ const ImageSearch = () => {
     }
     return (
         <div className='flex'>
-            <input className='bg-gray-50 border border-gray-300 text-sm w-full indent-2 py-2.5 px-2.5 outline-none focus:border-blue-500 focus:ring-2 rounded' type='search' value={value} placeholder='Search for image. . . ' onChange={e => handleChange(e)} />
+            <input className='bg-gray-50 border border-gray-300 text-sm w-full text-dark indent-2 py-2.5 px-2.5 outline-none focus:border-blue-500 focus:ring-2 rounded' type='search' value={value} placeholder='Search for image. . . ' onChange={e => handleChange(e)} />
             {/* <button className='bg-blue-600 px-5 py-2.5 text-white rounded-tr rounded-br focus:ring-2 focus:ring-blue-300 disable:bg-gray-400'>Search</button> */}
         </div>
     );
